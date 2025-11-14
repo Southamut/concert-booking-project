d0b3ae9faa36eb70902301e9747ddd7fc70609b8
Gamut <dovan.comm@gmail.com>
2025-11-14 16:37:33 +0700

    feat: add reservations module with controller, service, and DTO for concert reservations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConcertsModule } from './concerts/concerts.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [ConcertsModule],
+  imports: [ConcertsModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
