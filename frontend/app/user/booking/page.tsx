b89920cca9358b05c01f4a08fdce0a1143dc6b91
Gamut <dovan.comm@gmail.com>
2025-11-16 02:47:22 +0700

    feat: replace booking page card with ConcertCard component

--- frontend/app/user/booking/page.tsx
+++ frontend/app/user/booking/page.tsx
@@ -1,28 +1,12 @@
 import { UserLayout } from "@/components/layout/UserLayout";
-import { Card, CardTitle, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { User } from "lucide-react";
+import {ConcertCard} from "@/components/card/ConcertCard";
 
 export default function BookingPage() {
   return (
     <UserLayout>
       <div className="pt-10 sm:px-4 px-0">
         <div className="flex flex-col max-w-6xl mx-auto">
-          <Card className="bg-white border border-[#C2C2C2] shadow-none sm:p-10 p-4 sm:gap-8 gap-10">
-            <div className="flex flex-col gap-6">
-              <CardTitle className="border-b border-[#C2C2C2] sm:pb-6 pb-4 sm:text-[40px] text-2xl font-semibold text-[#1692EC] text-center sm:text-left">Concert 1</CardTitle>
-              <CardContent className="p-0">
-                <p className="sm:text-2xl text-base font-normal text-black">Lorem ipsum dolor sit amet consectetur. Elit purus nam gravida porttitor nibh urna sit ornare a. Proin dolor morbi id ornare aenean non. Fusce dignissim turpis sed non est orci sed in. Blandit ut purus nunc sed donec commodo morbi diam scelerisque.</p>
-              </CardContent>
-            </div>
-            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-0">
-              <div className="flex items-center gap-2">
-              <User className="w-8 h-8" />
-              <p className="text-2xl font-normal">100</p>
-              </div>
-              <Button className="sm:w-[160px] w-full h-15 bg-[#1692EC] text-white py-6 px-4 rounded-md text-2xl font-semibold">Reserve</Button>
-            </div>
-          </Card>
+          <ConcertCard />
         </div>
       </div>
     </UserLayout>
